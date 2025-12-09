6c2b3e78d897a2c2e4c33d243a9dbdaf08497b07
Will <williamtparsons@outlook.com>
2025-12-09 10:18:32 +0000

    Add files via upload

--- hangman code.py
+++ hangman code.py
@@ -1,5 +1,7 @@
 import requests
 
+whitesspace = "\n" * 50
+
 def get_random_word():
     url = "https://random-word-api.herokuapp.com/word"
     response = requests.get(url)
@@ -14,38 +16,59 @@ def user_input():
     while True:
         user_word = input("Enter a word (at least 3 letters): ").strip()
         if len(user_word) >= 3 and user_word.isalpha():
+            print(whitesspace)
             return user_word
         else:
             print("Invalid input. Please enter a valid word with at least 3 letters.")
 
 def main(input_word):
-    attempts = 6
+    correct = False
     word = input_word.title()
     reaveled = ['_'] * len(word)
-    
-    while attempts > 0 and '_' in reaveled:
+    incorrect = []
+
+    while len(incorrect) < 11 and '_' in reaveled:
 
         guess = input("Guess a letter: ").strip().lower()
         if guess.isalpha() and len(guess) == 1:
+            correct = False
+
             for i, letter in enumerate(word.lower()):
                 if letter == guess:
                     reaveled[i] = guess.upper()
+                    correct = True
+
+            if correct == False:
+                incorrect.append(guess)
+                print(f"Incorrect guesses: {(incorrect)}")
+                print(f"Attempts remaining: {11 - len(incorrect)}")
+                        
+            correct == False
             print("Current word: " + ' '.join(reaveled))
-            attempts -= 1
         else:
             print("Invalid guess. Please enter a single letter.")
+    
+    if len(incorrect) == 11:
+        print(f"You lost! The word was: {word}")
+    elif '_' not in reaveled:
+        print(f"Congratulations! You guessed the word: {word}")
+    else:
+        print("Error occurred.")
+
+        
         
 
 while True:
     game_mode = input("Choose game mode - '1' for random word, '2' for user-defined word: ").strip()
     if game_mode == '1':
         secret_word = get_random_word()
-        print(f"Random word selected: {secret_word}")
+        #print(f"Random word selected: {secret_word}")
         main(secret_word)
 
     elif game_mode == '2':
         secret_word = user_input()
-        print(f"User-defined word accepted: {secret_word}")
+        #print(f"User-defined word accepted: {secret_word}")
+        main(secret_word)
     else:
         print("Invalid game mode selected.")
 
