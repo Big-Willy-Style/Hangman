6bb1bd1653b4a6e7aec8ea5c0ec86b464c4ad2a3
Will <williamtparsons@outlook.com>
2025-12-09 09:50:34 +0000

    Add files via upload

---
+++ hangman code.py
@@ -0,0 +1,51 @@
+import requests
+
+def get_random_word():
+    url = "https://random-word-api.herokuapp.com/word"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        word = response.json()[0]
+        return word
+    else:
+        raise Exception("Failed to fetch word")
+    
+def user_input():
+    while True:
+        user_word = input("Enter a word (at least 3 letters): ").strip()
+        if len(user_word) >= 3 and user_word.isalpha():
+            return user_word
+        else:
+            print("Invalid input. Please enter a valid word with at least 3 letters.")
+
+def main(input_word):
+    attempts = 6
+    word = input_word.title()
+    reaveled = ['_'] * len(word)
+    
+    while attempts > 0 and '_' in reaveled:
+
+        guess = input("Guess a letter: ").strip().lower()
+        if guess.isalpha() and len(guess) == 1:
+            for i, letter in enumerate(word.lower()):
+                if letter == guess:
+                    reaveled[i] = guess.upper()
+            print("Current word: " + ' '.join(reaveled))
+            attempts -= 1
+        else:
+            print("Invalid guess. Please enter a single letter.")
+        
+
+while True:
+    game_mode = input("Choose game mode - '1' for random word, '2' for user-defined word: ").strip()
+    if game_mode == '1':
+        secret_word = get_random_word()
+        print(f"Random word selected: {secret_word}")
+        main(secret_word)
+
+    elif game_mode == '2':
+        secret_word = user_input()
+        print(f"User-defined word accepted: {secret_word}")
+    else:
+        print("Invalid game mode selected.")
+
